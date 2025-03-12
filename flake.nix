b5d542fb239f548ab9e612417a001fe88c8e56c1
Tobias Nichols <109690594+n1chols@users.noreply.github.com>
2025-03-12 00:10:15 -0700

    Update flake.nix

--- flake.nix
+++ flake.nix
@@ -1,32 +1,73 @@
 {
   inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
-  outputs = { nixpkgs }: {
-    nixosModules.steamUI = import ./steamUI.nix;
-    nixosModules.session-switcher = import ./session-switcher.nix;
-  };
-}
-{ config, pkgs, ... }: {
-
-  security.wrappers = {
-    gamescope = {
-      owner = "root";
-      group = "root";
-      source = "${pkgs.gamescope}/bin/gamescope";
-      capabilities = "cap_sys_nice+eip";
+  outputs = { ... }: {
+    nixosModules.default = { config, lib, pkgs, ... }: let
+      cfg = config.steam-console;
+    in {
+      options.steam-console = {
+        enable = lib.mkOption {
+          type = lib.types.bool;
+          default = false;
+        };
+        enableDecky = lib.mkOption {
+          type = lib.types.bool;
+          default = false;
+        };
+        user = lib.mkOption {
+          type = lib.types.str;
+        };
+        desktopSession = lib.mkOption {
+          type = lib.types.str;
+          default = "steam-session";
+        };
+      };
+      config = lib.mkIf cfg.enable {
+        security.wrappers = {
+          gamescope = {
+            owner = "root";
+            group = "root";
+            source = "${pkgs.gamescope}/bin/gamescope";
+            capabilities = "cap_sys_nice+eip";
+          };
+          bwrap = {
+            owner = "root";
+            group = "root";
+            source = "${pkgs.bubblewrap}/bin/bwrap";
+            setuid = true;
+          };
+        };
+        environment.systemPackages = [
+          (pkgs.writeShellScriptBin "steam-session" ''
+            #!/bin/sh
+            exec ${config.security.wrapperDir}/gamescope -f -e -- ${pkgs.steam}/bin/steam -tenfoot -steamos3 > /dev/null 2>&1
+          '')
+          pkgs.steam
+          pkgs.gamescope
+        ];
+        systemd.tmpfiles.rules = [
+          "d /home/${cfg.user}/.local/bin 0755 ${cfg.user} ${cfg.user} -"
+          "L /home/${cfg.user}/.local/bin/steamos-session-select - - - - ${
+            pkgs.writeTextFile {
+              name = "steamos-session-select";
+              text = ''
+                #!/bin/sh
+                steam -shutdown
+                ${cfg.desktopSession}
+              '';
+              executable = true;
+            }
+          }"
+        ];
+        services.greetd = {
+          enable = true;
+          settings = {
+            default_session = {
+              user = cfg.user;
+              command = "steam-session";
+            };
+          };
+        };
+      };
     };
-    bwrap = {
-      owner = "root";
-      group = "root";
-      source = "${pkgs.bubblewrap}/bin/bwrap";
-      setuid = true;
-    };
   };
-
-  environment.systemPackages = [
-    (pkgs.writeShellScriptBin "steamscope" ''
-      #!/bin/sh
-      exec ${config.security.wrapperDir}/gamescope -f -e "$@" -- ${(pkgs.steam.override { buildFHSEnv = pkgs.buildFHSEnv.override { bubblewrap = "${config.security.wrapperDir}/.."; }; })}/bin/steam -tenfoot > /dev/null 2>&1
-    '')
-  ];
-
 }
