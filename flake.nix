ed66f7633999ca5232a1afd745d2ac4d16d4fde9
Tobias Nichols <109690594+n1chols@users.noreply.github.com>
2025-03-12 21:11:28 -0700

    Update flake.nix

--- flake.nix
+++ flake.nix
@@ -113,7 +113,7 @@
         (lib.mkIf cfg.enableDecky {
           systemd.tmpfiles.rules = [
             # Enable CEF remote debugging
-            "f /home/${cfg.user}/.steam/steam/.cef-enable-remote-debugging 0644 root root -"
+            "f /home/${cfg.user}/.local/share/Steam/.cef-enable-remote-debugging 0644 ${cfg.user} users -"
             # Set ownership of /var/lib/decky
             "d /var/lib/decky 0755 decky decky -"
           ];
