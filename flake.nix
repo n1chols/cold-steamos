4d88b8b03a99adb988a679d1409eab50b95d56a3
Tobias Nichols <109690594+n1chols@users.noreply.github.com>
2025-03-15 02:12:49 -0700

    Update flake.nix

--- flake.nix
+++ flake.nix
@@ -1,5 +1,5 @@
 {
-  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/release-24.11";
 
   outputs = { ... }: {
     nixosModules.default = { config, lib, pkgs, ... }: let
