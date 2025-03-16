1ad63e8ebdba6b40cf3840ae230b36453efa1c86
Tobias Nichols <109690594+n1chols@users.noreply.github.com>
2025-03-15 22:35:03 -0700

    Update flake.nix

--- flake.nix
+++ flake.nix
@@ -71,7 +71,7 @@
                     bubblewrap = "${config.security.wrapperDir}/..";
                   };
                 })}/bin/steam \
-                -tenfoot -steamos3 -pipewire-dmabuf -skipinitialbootstrap \
+                -tenfoot -steamos3 -pipewire-dmabuf \
                 > /dev/null 2>&1
               fi
             '')
