84e482a1d8840b9a82ec8c1f4937b2e8372a5950
Tobias Nichols <109690594+n1chols@users.noreply.github.com>
2025-03-15 17:51:31 -0700

    Update flake.nix
    switch to steam module for testing purposes

--- flake.nix
+++ flake.nix
@@ -49,6 +49,19 @@
               setuid = true;
             };
           };
+
+          # Enable steam with setuid bubblewrap package
+          programs.steam = {
+            enable = true;
+            remotePlay.openFirewall = true;
+            dedicatedServer.openFirewall = true;
+            localNetworkGameTransfers.openFirewall = true;
+            package = pkgs.steam.override {
+              buildFHSEnv = pkgs.buildFHSEnv.override {
+                bubblewrap = "${config.security.wrapperDir}/..";
+              };
+            };
+          };
   
           # Install steam-session script with wrappers
           environment.systemPackages = [
@@ -66,11 +79,7 @@
                   "--immediate-flips"
                 ] ++ lib.optionals cfg.enableHDR [ "--hdr-enabled" "--hdr-itm-enable" ]
                   ++ lib.optionals cfg.enableVRR [ "--adaptive-sync" ])} -- \
-                ${(pkgs.steam.override {
-                  buildFHSEnv = pkgs.buildFHSEnv.override {
-                    bubblewrap = "${config.security.wrapperDir}/..";
-                  };
-                })}/bin/steam \
+                ${pkgs.steam}/bin/steam \
                 -tenfoot -steamos3 -pipewire-dmabuf
               fi
             '')
